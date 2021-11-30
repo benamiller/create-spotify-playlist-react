b02cf87ca797bce9264b57f0e94d1b73f1005ff5
Hactually Benji <mrbenamiller@gmail.com>
2021-11-30 12:21:15 -0800

    Overall structure created for application

---
+++ src/Components/App/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults';
+import { Playlist } from '../Playlist/Playlist';
+
+function App() {
+  return (
+    <div>
+      <h1>Spot<span class="highlight">iii</span>ify</h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults />
+          <Playlist />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
